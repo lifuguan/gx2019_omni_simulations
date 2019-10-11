007449907f77d7ed0373afa38f87cbdcd3a0abee
lifuguan <1002732355@qq.com>
2019-10-11 20:57:21 +0800

    完成识别， 还没有计算像素偏差值与角度关系

--- src/gx2019_cylinder_localization_node.cpp
+++ src/gx2019_cylinder_localization_node.cpp
@@ -1,120 +1,201 @@
-#include <visp3/core/vpIoTools.h>
-#include <visp3/gui/vpDisplayGDI.h>
-#include <visp3/gui/vpDisplayOpenCV.h>
-#include <visp3/gui/vpDisplayX.h>
-#include <visp3/io/vpImageIo.h>
-#include <visp3/mbt/vpMbEdgeKltTracker.h>
-#include <visp3/mbt/vpMbEdgeTracker.h>
-#include <visp3/io/vpVideoReader.h>
-
+/*
+ * @Description: In User Settings Edit
+ * @Author: lifuguan
+ * @Date: 2019-10-03 15:21:38
+ * @LastEditTime: 2019-10-11 20:37:24
+ * @LastEditors: Please set LastEditors
+ */
+#include <iostream>
+#include <string>
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
 
+using namespace std;
 using namespace cv;
 
 static const std::string IMAGE_TOPIC = "/my_robot/camera1/image_raw";
 
-vpDisplayX *d = NULL;
+//设置颜色区间
+Scalar red_range[3] = {Scalar(0, 160, 50), Scalar(10, 255, 255)};
+Scalar green_range[3] = {Scalar(25, 189, 118), Scalar(95, 255, 198)};
+Scalar blue_range[3] = {Scalar(98, 102, 20), Scalar(130, 255, 255)};
 
-vpImage<unsigned char> I;
-// 摄像头标定数据
-vpCameraParameters cam;
-// 被识别物体的位姿
-vpHomogeneousMatrix cMo;
-
-vpMbTracker *tracker;
-vpMe me;
-vpKltOpencv klt_settings;
+int target_color = 0;
+enum TARGET_COLOR
+{
+    red,
+    blue,
+    green
+};
 
 void imageCallback(const sensor_msgs::ImageConstPtr &msg);
+//初始化图形并圈出
+string detect(vector<Point> cnts_single, vector<Point> &approx);
+
+void drawLine(Mat &frame, string shape, vector<Point> &approx);
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "gx2019_mission_control_node");
+    ros::init(argc, argv, "gx2019_cylinder_localization_node");
     ros::NodeHandle nh;
     image_transport::ImageTransport it(nh);
     image_transport::Subscriber sub = it.subscribe(IMAGE_TOPIC, 1, imageCallback);
-    
-
-   
+    namedWindow("OPENCV_WINDOW");
+    cout << blue_range[0][0] << endl;
     // 等待函数
     ros::spin();
     return 0;
 }
 
-int time_ = 1;
-
 void imageCallback(const sensor_msgs::ImageConstPtr &msg)
 {
-    Mat frame;
-    vpImage<unsigned char> I; // for gray images
-    ROS_INFO("INSIDE.");
+    Mat frame_hsv, frame;
     try
     {
         frame = cv_bridge::toCvShare(msg, "bgr8")->image;
-        // 第一次进入回调 ， 初始化现实界面
-        if (time_ == 1)
-        {
-            vpImageConvert::convert(frame, I);
-            d = new vpDisplayX();
-            d->init(I);
-            vpDisplay::setTitle(I, "visp_auto_tracker debug display");
-             // 初始化配置
-#if (opt_tracker == 0)
-    tracker = new vpMbEdgeTracker;
-#elif (opt_tracker == 1)
-    tracker = new vpMbKltTracker;
-#else
-    tracker = new vpMbEdgeKltTracker;
-#endif
-    me.setMaskSize(5);
-    me.setMaskNumber(180);
-    me.setRange(8);
-    me.setThreshold(10000);
-    me.setMu1(0.5);
-    me.setMu2(0.5);
-    me.setSampleStep(4);
-    dynamic_cast<vpMbEdgeTracker *>(tracker)->setMovingEdge(me);
-
-    klt_settings.setMaxFeatures(300);
-    klt_settings.setWindowSize(5);
-    klt_settings.setQuality(0.015);
-    klt_settings.setMinDistance(8);
-    klt_settings.setHarrisFreeParameter(0.01);
-    klt_settings.setBlockSize(3);
-    klt_settings.setPyramidLevels(3);
-
-    dynamic_cast<vpMbKltTracker *>(tracker)->setKltOpencv(klt_settings);
-    dynamic_cast<vpMbKltTracker *>(tracker)->setKltMaskBorder(5);
-    ROS_INFO("INSIDE.!!");
-
-    std::string objectname = "cylinder";
-    cam.initPersProjWithoutDistortion(839, 839, 325, 243);
-    tracker->setCameraParameters(cam);
-    tracker->loadModel(objectname + ".cao");
-    tracker->setDisplayFeatures(true);
-    // tracker->initClick(I, objectname + ".init", true);
-
-            time_ += 1;
-        }
-        // 转换OpenCV到VISP
-        vpImageConvert::convert(frame, I);
-        vpDisplay::display(I);
-        vpDisplay::flush(I);
-
-        tracker->track(I);
-        tracker->getPose(cMo);
-        tracker->getCameraParameters(cam);
-        tracker->display(I, cMo, cam, vpColor::red, 2, true);
-        vpDisplay::displayFrame(I, cMo, cam, 0.025, vpColor::none, 3);
-        vpDisplay::displayText(I, 10, 10, "A click to exit...", vpColor::red);
-        vpDisplay::flush(I);
-
-        vpTime::wait(40);
     }
-    catch (const std::exception &e)
+    catch (cv_bridge::Exception &e)
     {
+        ROS_ERROR("%s", e.what());
+        return;
+    }
+
+    //图像格式切换
+    cvtColor(frame, frame_hsv, COLOR_BGR2HSV);
+
+    Mat target_frame;
+    if (target_color == red)
+    {
+        inRange(frame_hsv, red_range[0], red_range[1], target_frame);
+    }
+    else if (target_color == blue)
+    {
+        inRange(frame_hsv, blue_range[0], blue_range[1], target_frame);
+    }
+    else
+    {
+        inRange(frame_hsv, green_range[0], green_range[1], target_frame);
+    }
+
+    //
+    Mat erode_kernel = getStructuringElement(MORPH_RECT, Size(2, 2));
+    erode(target_frame, target_frame, erode_kernel, Point(-1, -1), 1);
+    //
+    Mat dilate_kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
+    dilate(target_frame, target_frame, dilate_kernel, Point(-1, -1), 1);
+
+    vector<vector<Point>> cnts; //获取了一堆又一堆点
+    findContours(target_frame, cnts, RETR_EXTERNAL, CHAIN_APPROX_NONE);
+
+    for (int i = 0; i < cnts.size(); i++)
+    {
+        vector<Point> cnts_single = cnts[i]; //获取了上面一堆点中的一个点
+        if (cnts_single.size() > 50)
+        {
+            vector<Point> approx;
+            string shape = detect(cnts_single, approx);
+            Moments M = moments(cnts_single);
+            int cX, cY;
+            if (M.m10 != 0)
+            {
+                //表示图像重心
+                cX = int((M.m10 / M.m00));
+                cY = int((M.m01 / M.m00));
+            }
+            else
+            {
+                cX = cY = 0;
+            }
+            //画中位线
+            line(frame, Point(frame.cols / 2, 0), Point(frame.cols / 2, frame.rows), (255, 255, 255), 2);
+            //画质心线
+            line(frame, Point(cX, cY), Point(0, cY), Scalar(255, 255, 255), 1);
+            line(frame, Point(cX, cY), Point(cX, 0), Scalar(255, 255, 255), 1);
+
+            putText(frame, "black line " + shape, Point(cX, cY), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 255), 2);
+            line(frame, Point(cX, cY), Point(cX, cY), (255, 255, 255), 5);
+            putText(frame, "position " + to_string(cX) + " , " + to_string(cY), Point(cX, cY + 20), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(255, 0, 255), 1); //质心位置
+            drawLine(frame, shape, approx);                                                                                                             //画矩形
+            imshow("OPENCV_WINDOW", frame);
+        }
+        else
+        {
+            //printf("too fucking small %d\n", cnts_single.size());
+        }
+    }
+
+    waitKey(30);
+}
+
+//初始化图形并圈出
+string detect(vector<Point> cnts_single, vector<Point> &approx)
+{
+    string shape = "undentified";
+    double peri = arcLength(cnts_single, true);
+    approxPolyDP(cnts_single, approx, 0.045 * peri, true);
+    if (approx.size() == 3)
+    {
+        shape = "triangle";
+    }
+    else if (approx.size() == 4)
+    {
+        shape = "rectangle";
+        /*待完成*/
+    }
+    else if (approx.size() == 5)
+    {
+        shape = "pentagon";
+    }
+    else
+    {
+        shape = "circle";
+    }
+
+    return shape;
+}
+
+void drawLine(Mat &frame, string shape, vector<Point> &approx)
+{
+    Point pt[13];
+    if (shape == "rectangle" || shape == "sqaure")
+    {
+        pt[1] = approx[0];
+        pt[2] = approx[1];
+        pt[3] = approx[2];
+        pt[4] = approx[3];
+        line(frame, pt[1], pt[2], Scalar(0, 255, 0), 2);
+        line(frame, pt[3], pt[2], Scalar(0, 255, 0), 2);
+        line(frame, pt[4], pt[3], Scalar(0, 255, 0), 2);
+        line(frame, pt[4], pt[1], Scalar(0, 255, 0), 2);
+    }
+    else if (shape == "pentagon")
+    {
+        pt[1] = approx[0];
+        pt[2] = approx[1];
+        pt[3] = approx[2];
+        pt[4] = approx[3];
+        pt[5] = approx[4];
+        line(frame, pt[1], pt[2], Scalar(0, 255, 0), 3);
+        line(frame, pt[3], pt[2], Scalar(0, 255, 0), 3);
+        line(frame, pt[4], pt[3], Scalar(0, 255, 0), 3);
+        line(frame, pt[4], pt[5], Scalar(0, 255, 0), 3);
+        line(frame, pt[5], pt[1], Scalar(0, 255, 0), 3);
+    }
+    else if (shape == "cross")
+    {
+        pt[1] = approx[0];
+        pt[2] = approx[1];
+        pt[3] = approx[2];
+        pt[4] = approx[3];
+        pt[5] = approx[4];
+        pt[6] = approx[5];
+        line(frame, pt[1], pt[2], Scalar(0, 255, 0), 3);
+        line(frame, pt[3], pt[2], Scalar(0, 255, 0), 3);
+        line(frame, pt[4], pt[3], Scalar(0, 255, 0), 3);
+        line(frame, pt[4], pt[5], Scalar(0, 255, 0), 3);
+        line(frame, pt[5], pt[6], Scalar(0, 255, 0), 3);
+        line(frame, pt[1], pt[1], Scalar(0, 255, 0), 3);
     }
 }
